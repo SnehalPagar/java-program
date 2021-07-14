ae7d189ef827fcf16c1970bfa3dc321f66bdba11
snehal07 <86036804+SnehalPagar@users.noreply.github.com>
2021-07-14 15:32:15 +0530

    Add files via upload

---
+++ number.java
@@ -0,0 +1,13 @@
+simport java.util.Scanner;
+class number
+{
+	public static void main(String [] args)
+  	{
+   		int x;
+   		Scanner Scanner;
+   		Scanner=new Scanner(System.in);
+   		System.out.println("Enter number:");
+   		x=Scanner.nextInt();
+   		System.out.println(" "+ x +x +x +x +x +" ");
+  	}
+}
